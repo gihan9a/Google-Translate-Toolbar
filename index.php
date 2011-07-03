6e53b7071f11a2149cc4c2d7a435e2afb85674f1
Gihan Subhashana <gihanshp@gmail.com>
2011-07-03 19:36:57 +0530

    added GNU GPL License information.

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php
 /*
+ *      index.php
+ *      
+ *      Copyright 2011 Gihan Subhashana <gihanshp@gmail.com>
+ *      
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *      
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *      
+ */
+
+/*
 Plugin Name: Google Translate Toolbar
 Plugin URI: https://github.com/gihanshp/Google-Translate-Toolbar
 Description: Adds Google translate toolbar to your wordpress blog/site. This plugin supports more than 40 languages translation
